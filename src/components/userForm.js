179f69fcb8f55e44bc434f9d128395feb8f8af1c
Andrew Johnson <ajohnson.uva@gmail.com>
2019-05-03 13:23:12 -0400

    add form validations

--- src/components/userForm.js
+++ src/components/userForm.js
@@ -1,28 +1,126 @@
 import React, { Component } from 'react';
+// import { Formik } from 'formik';
+// import * as yup from 'yup';
 import { connect } from 'react-redux'
+// import { Field, reduxForm } from 'redux-form'
 import createUser from '../actions/createUser'
 import { Container, Form, InputGroup, Button, Row, Col } from 'react-bootstrap'
 
-class UserForm extends Component {
-  state = { user: { zipcode: '', salary: '', rent_cost: '', utilities: '', bedrooms: ''}}
+// const schema = yup.object({
+//   zipcode: yup.string().length(5).required(),
+//   salary: yup.string().required(),
+//   rent_cost: yup.string().required(),
+//   utilities: yup.string()
+// })
 
-  handleChange = event => { this.setState({ user: { ...this.state.user, [event.target.name]: event.target.value }})}
+class UserForm extends Component {
+  state = {
+    user: {
+      zipcode: '',
+      salary: '',
+      rent_cost: '',
+      utilities: '',
+      bedrooms: ''
+    },
+    validations: {
+      zipcodeValid: false,
+      salaryValid: false,
+      rent_costValid: false,
+      utilitiesValid: false
+    },
+    formErrors: {zipcode: '', salary: '', rent_cost: '', utilities: ''}
+  }
+
+  validateField = (fieldName, value) => {
+    const checkNumericalInput = (fieldName, value) => {
+      if (/[^\d]/.test(value) || value.length === 0) {
+        this.setState({
+          ...this.state,
+          validations: { ...this.state.validations, [fieldName + 'Valid']: false },
+          formErrors: { ...this.state.formErrors, [fieldName]: 'Please only include digits 0-9'}})
+      } else {
+        this.setState({
+          ...this.state,
+          validations: { ...this.state.validations, [fieldName + 'Valid']: true },
+          formErrors: { ...this.state.formErrors, [fieldName]: 'Looks good!'}
+        })
+      }
+    }
+    switch (fieldName) {
+      case "zipcode":
+        if (/[^\d]/.test(value) || !(value.length === 5)) {
+          this.setState({
+            ...this.state,
+            validations: { ...this.state.validations, zipcodeValid: false },
+            formErrors: { ...this.state.formErrors, zipcode: 'Must include valid Zip Code (e.g. 27103)'}})
+        } else {
+          this.setState({
+            ...this.state,
+            validations: { ...this.state.validations, zipcodeValid: true },
+            formErrors: { ...this.state.formErrors, zipcode: 'Looks good!'}
+          })
+        }
+        break;
+      case "salary":
+        checkNumericalInput(fieldName, value);
+        break;
+      case "rent_cost":
+        checkNumericalInput(fieldName, value);
+        break;
+      case "utilities":
+        checkNumericalInput(fieldName, value);
+        break;
+    }
+  }
+
+  handleChange = event => {
+    const name = event.target.name;
+    const value = event.target.value
+    this.setState({ user: { ...this.state.user, [name]: value }},
+      () => { this.validateField(name, value); console.log(this.state) })
+  }
 
   handleSubmit = event => {
     event.preventDefault();
-    this.props.createUser(this.state);
-    this.setState({ user: { zipcode: '', salary: '', rent_cost: '', utilities: '', bedrooms: ''}})
+    // const form = event.currentTarget;
+    // if (form.checkValidity() === false) {
+    //   event.stopPropagation();
+    // }
+    if (Object.values(this.state.validations).every(value => value === true)) {
+      this.props.createUser(this.state);
+      this.setState({ user: { zipcode: '', salary: '', rent_cost: '', utilities: '', bedrooms: ''}})
+    } else {
+      alert("Please confirm that all form elements are valid.")
+    }
   }
+  //
+  // onBlur = ({
+  //   zipcode: (event) => {
+  //     if (this.state.user.zipcode.match)
+  //   }
+  // })
 
   render() {
     return(
       <Container id="formContainer">
-        <Form onSubmit={this.handleSubmit}>
+        <Form noValidate onSubmit={this.handleSubmit}>
           <Row>
             <Col>
               <Form.Group controlId="zipcode">
                 <Form.Label>Zip Code</Form.Label>
-                <Form.Control type="text" name="zipcode" placeholder="Enter your zip code here..." value={this.state.user.zipcode} onChange={this.handleChange} required/>
+                <Form.Control
+                  type="text"
+                  name="zipcode"
+                  className={this.state.formErrors.zipcode ? (this.state.validations.zipcodeValid ? 'is-valid' : 'is-invalid') : ''}
+                  placeholder="Enter your zip code here..."
+                  value={this.state.user.zipcode}
+                  onChange={this.handleChange}
+                  required/>
+                {this.state.formErrors.zipcode ? null : <Form.Text className="text-muted">
+                  We don't share personal information with anyone.
+                </Form.Text>}
+                <Form.Control.Feedback type="valid">Looks Good!</Form.Control.Feedback>
+                <Form.Control.Feedback type="invalid">{this.state.formErrors.zipcode}</Form.Control.Feedback>
               </Form.Group>
             </Col>
             <Col>
@@ -32,11 +130,18 @@ class UserForm extends Component {
                   <InputGroup.Prepend>
                     <InputGroup.Text id="SalaryPrepend">$</InputGroup.Text>
                   </InputGroup.Prepend>
-                  <Form.Control type="text" name="salary" placeholder="Enter your annual income (before taxes)..." aria-describedby="SalaryPrepend" value={this.state.user.salary} onChange={this.handleChange} required/>
+                  <Form.Control
+                    type="text"
+                    name="salary"
+                    className={this.state.formErrors.salary ? (this.state.validations.salaryValid ? 'is-valid' : 'is-invalid') : ''}
+                    placeholder="Enter your annual income (before taxes)..."
+                    aria-describedby="SalaryPrepend"
+                    value={this.state.user.salary}
+                    onChange={this.handleChange}
+                    required/>
+                  <Form.Control.Feedback type="valid">Looks Good!</Form.Control.Feedback>
+                  <Form.Control.Feedback type="invalid">{this.state.formErrors.salary}</Form.Control.Feedback>
                 </InputGroup>
-                <Form.Text className="text-muted">
-                  We don't share personal information with anyone.
-                </Form.Text>
               </Form.Group>
             </Col>
           </Row>
@@ -49,7 +154,16 @@ class UserForm extends Component {
                   <InputGroup.Prepend>
                     <InputGroup.Text id="RentCostPrepend">$</InputGroup.Text>
                   </InputGroup.Prepend>
-                  <Form.Control type="text" name="rent_cost" placeholder="Ex. 800" aria-describedby="RentCostPrepend" value={this.state.user.rentCost} onChange={this.handleChange} required/>
+                  <Form.Control
+                    type="text"
+                    name="rent_cost"
+                    className={this.state.formErrors.rent_cost ? (this.state.validations.rent_costValid ? 'is-valid' : 'is-invalid') : ''}
+                    placeholder="Ex. 800"
+                    aria-describedby="RentCostPrepend"
+                    value={this.state.user.rent_cost}
+                    onChange={this.handleChange} required/>
+                  <Form.Control.Feedback type="valid">Looks Good!</Form.Control.Feedback>
+                  <Form.Control.Feedback type="invalid">{this.state.formErrors.rent_cost}</Form.Control.Feedback>
                 </InputGroup>
               </Form.Group>
             </Col>
@@ -60,19 +174,32 @@ class UserForm extends Component {
                   <InputGroup.Prepend>
                     <InputGroup.Text id="UtilitiesPrepend">$</InputGroup.Text>
                   </InputGroup.Prepend>
-                  <Form.Control type="text" name="utilities" placeholder="(e.g. water, electricity, etc.)" aria-describedby="UtilitiesPrepend" value={this.state.user.utilities} onChange={this.handleChange} required/>
+                  <Form.Control
+                    type="text"
+                    name="utilities"
+                    className={this.state.formErrors.utilities ? (this.state.validations.utilitiesValid ? 'is-valid' : 'is-invalid') : ''}
+                    placeholder="(e.g. water, electricity, etc.)"
+                    aria-describedby="UtilitiesPrepend"
+                    value={this.state.user.utilities}
+                    onChange={this.handleChange} required/>
+                  <Form.Control.Feedback type="valid">Looks Good!</Form.Control.Feedback>
+                  <Form.Control.Feedback type="invalid">{this.state.formErrors.utilities}</Form.Control.Feedback>
                 </InputGroup>
               </Form.Group>
             </Col>
             <Col>
               <Form.Group controlId="bedrooms">
                 <Form.Label># of Bedrooms Rented</Form.Label>
-                <Form.Control as="select" name="bedrooms" value={this.state.user.bedrooms} onChange={this.handleChange}>
-                  <option value="0">Studio/Efficiency</option>
-                  <option value="1">1 bedroom</option>
-                  <option value="2">2 bedrooms</option>
-                  <option value="3">3 bedrooms</option>
-                  <option value="4">4 bedrooms</option>
+                <Form.Control
+                  as="select"
+                  name="bedrooms"
+                  value={this.state.user.bedrooms}
+                  onChange={this.handleChange}>
+                    <option value="0">Studio/Efficiency</option>
+                    <option value="1">1 bedroom</option>
+                    <option value="2">2 bedrooms</option>
+                    <option value="3">3 bedrooms</option>
+                    <option value="4">4 bedrooms</option>
                 </Form.Control>
               </Form.Group>
             </Col>
