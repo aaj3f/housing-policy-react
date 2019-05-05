30661b2db33855d40afb78efb9fccf3b864c4a00
Andrew Johnson <ajohnson.uva@gmail.com>
2019-05-05 12:31:57 -0400

    add rendering logic for first column of UserContainer Component

--- src/containers/userContainer.js
+++ src/containers/userContainer.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class UserContainer extends Component {
+
+  renderMain = () => {
+    let { warren, booker, harris } = this.props.user
+    let h2, p
+    if ([warren, booker, harris].some(value => !!value)) {
+      h2 = "You Would Qualify for Benefits Under:"
+      p = ''
+      p += (!!warren ? "Elizabeth Warren's Policy" : '')
+      p += (!!booker ? "<br/>Cory Booker's Policy" : '')
+      p += (!!harris ? "<br/>Kamala Harris' Policy" : '')
+    } else {
+      h2 = "You Don't Seem to Qualify for Benefits Under Any of the Proposed Policies..."
+      p = "You can learn more about the policies & their eligibility conditions by clicking on them here."
+    }
+    return(
+      <span className="caption-content">
+        <h2>{h2}</h2>
+        <p className="mb-0">{p}</p>
+      </span>
+    )
+  }
 
   render() {
     return(
       <section className="content-section" id="portfolio">
         <div className="container">
-        <div className="row no-gutters">
-          <div className="col-lg-6">
-            <a className="portfolio-item" href="#">
-              <span className="caption">
-                <span className="caption-content">
-                  <h2>Stationary</h2>
-                  <p className="mb-0">A yellow pencil with envelopes on a clean, blue backdrop!</p>
+          <div className="row no-gutters">
+            <div className="col-lg-6">
+              <a className="portfolio-item" href="#">
+                <span className="caption">
+                  {this.renderMain()}
                 </span>
-              </span>
               <img className="img-fluid" src={require("../img/american-sticker.jpg")} alt="" />
             </a>
           </div>
@@ -58,4 +77,10 @@ class UserContainer extends Component {
   }
 }
 
-export default UserContainer
+const mapStateToProps = state => {
+  return({
+    user: state.userReducer.user
+  })
+}
+
+export default connect(mapStateToProps)(UserContainer)
