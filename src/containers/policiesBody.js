a336571754b8acfa01f34d8bb45c615c939278c8
Andrew Johnson <ajohnson.uva@gmail.com>
2019-05-10 09:43:56 -0400

    add basic layout / styling to PoliciesBody container

--- src/containers/policiesBody.js
+++ src/containers/policiesBody.js
@@ -5,7 +5,14 @@ class PoliciesBody extends Component {
 
   render() {
     return(
-      <h1>Policies Body</h1>
+      <section class="content-section bg-primary" id="policy-selector">
+        <div class="policies-selection container text-center">
+          <h2 class="mb-4">Whose housing policy are you interested in reading about...</h2>
+          <a href="#" class="btn btn-xl btn-dark mx-2 mb-2">Elizabeth Warren</a>
+          <a href="#" class="btn btn-xl btn-dark mx-2 mb-2">Cory Booker</a>
+          <a href="#" class="btn btn-xl btn-dark mx-2 mb-2">Kamala Harris</a>
+        </div>
+      </section>
     )
   }
 }
