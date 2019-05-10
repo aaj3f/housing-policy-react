864b4a8086a8fbcac234ccd3462c0ad7d6affe8b
Andrew Johnson <ajohnson.uva@gmail.com>
2019-05-10 09:03:41 -0400

    stub PoliciesContainer

---
+++ src/containers/policiesContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class PoliciesContainer extends Component {
+
+  render() {
+    return(<h1>Policies Container</h1>)
+  }
+}
+
+export default connect()(PoliciesContainer)
