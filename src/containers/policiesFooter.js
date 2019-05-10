30644b0b1ce47b2c2169a4e2a987fa7eab6d2c1a
Andrew Johnson <ajohnson.uva@gmail.com>
2019-05-10 18:59:44 -0400

    edit componentDidMount scrollDown

--- src/containers/policiesFooter.js
+++ src/containers/policiesFooter.js
@@ -6,7 +6,7 @@ class PoliciesFooter extends Component {
   scrollViewportDown() {
     setTimeout(() => {
       document.querySelector('#policies-footer').scrollIntoView({ block: 'start', inline: 'nearest', behavior: 'smooth'})
-    }, 1000)
+    }, 100)
   }
 
   componentDidMount() {
@@ -17,24 +17,24 @@ class PoliciesFooter extends Component {
     this.scrollViewportDown()
   }
 
-  renderH3 = () => {
-    let h3
+  renderH2 = () => {
+    let h2
     switch (this.props.match.params.policyId) {
       case "warren":
-        h3 = "Elizabeth Warren's Policy"
+        h2 = "Elizabeth Warren's Policy"
         break;
       case "booker":
-        h3 = "Cory Booker's Policy"
+        h2 = "Cory Booker's Policy"
         break;
       case "harris":
-        h3 = "Kamala Harris' Policy"
+        h2 = "Kamala Harris' Policy"
         break;
       default:
-      h3 = null
+      h2 = "No Policy Exists by That Name"
     }
 
     return(
-      <h3 id="footer-secondary" className="mb-0">{h3}</h3>
+      <h2 className="mx-auto mb-5">{h2}</h2>
     )
   }
 
@@ -43,8 +43,8 @@ class PoliciesFooter extends Component {
       <section className="content-section bg-policies-footer text-white text-center" id="policies-footer">
         <div className="container">
           <div className="content-section-heading">
-            {this.renderH3()}
-            <h2 className="mx-auto mb-5">Info About Your Area Retrieved from<br/>The Dept of HUD & the US Census Bureau</h2>
+            <h3 id="footer-secondary" className="mb-0">Policy Info</h3>
+            {this.renderH2()}
           </div>
         </div>
       </section>
