8cd0cef64af5851f824f3dc29e4bc8c45883f389
Andrew Johnson <ajohnson.uva@gmail.com>
2019-05-12 11:32:33 -0400

    abstract block of JSX/HTML into separate UseEstimator functional component

--- src/containers/policiesHeader.js
+++ src/containers/policiesHeader.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import UseEstimator from '../components/useEstimator'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { Textfit } from 'react-textfit'
@@ -7,13 +8,7 @@ class PoliciesHeader extends Component {
 
   renderHeader = () => {
     if (Object.keys(this.props.user).length === 0) {
-      return(
-        <div className="content-section-heading">
-          <h3 className="text-secondary mb-0">Your Data</h3>
-          <h2 className="mx-auto mb-5">For Personalized Information About Your Neighborhood,<br/>Use Our Policy Benefits Estimator:</h2>
-          <Link to="/home" href="#" className="btn btn-xl btn-light">Let's do it!</Link>
-        </div>
-      )
+      return(<UseEstimator />)
     } else {
       let medianIncome = this.props.user.median_income
       let displayMedianIncome = this.displayNum(medianIncome)
