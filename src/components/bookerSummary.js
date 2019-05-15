80b4032a8f47879365d7990a8d825c9c50b862c9
Andrew Johnson <ajohnson.uva@gmail.com>
2019-05-15 15:25:42 -0400

    edit Summary component layout

--- src/components/bookerSummary.js
+++ src/components/bookerSummary.js
@@ -5,8 +5,8 @@ class BookerSummary extends Component {
   render() {
     return(
       <React.Fragment>
-        <h2>Booker's Main Policy Goals</h2>
-        <p>To provide rent relief to anyone who...<br/><br/><span style={{ fontSize: "1.4rem" }}>(1) pays more than 30% of their monthly income on rent (*)</span><br/><br/>Booker's policy additionally provides community development grants to ease zoning laws which restrict the construction of affordable housing<br/><br/>It also allows renters to defer portions of their tax credit to an interest-earning "Rainy Day" savings fund<br/><br/>(*) His policy does, however, provide a lower benefit to anyone who pays more for rent than the Fair Market Rent (FMR) evaluation for their area.</p>
+        <h2>Main Policy Goals</h2>
+        <p className="text-center text-justify">To provide rent relief to anyone who...<br/><br/><span style={{ fontSize: "1.4rem" }}>(1) pays more than 30% of their monthly income on rent (*)</span><br/><br/>Booker's policy additionally provides community development grants to ease zoning laws which restrict the construction of affordable housing<br/><br/>It also allows renters to defer portions of their tax credit to an interest-earning "Rainy Day" savings fund<br/><br/>(*) His policy does, however, provide a lower benefit to anyone who pays more for rent than the Fair Market Rent (FMR) evaluation for their area.</p>
       </React.Fragment>
     )
   }
--- src/components/warrenSummary.js
+++ src/components/warrenSummary.js
@@ -5,8 +5,8 @@ class WarrenSummary extends Component {
   render() {
     return(
       <React.Fragment>
-        <h2>Warren's Policy Aims To:</h2>
-        <p>[Policy Data Goes Here]</p>
+        <h2>Main Policy Goals</h2>
+        <p className="text-center text-justify">To provide downpayment assistance to anyone who...<br/><br/><span style={{ fontSize: "1.4rem" }}>(1) makes less than 120% the median income in their area<br/>(2) lives in a low-income or historically red-lined neighborhood</span><br/><br/>That downpayment also ensures Fair Housing Act (FHA) protections to new homebuyers to prevent unfair mortgage practices, and her policy expands on those protections for minorities and Section 8 recipients.<br/><br/>Her policy also dedicates $470 billion to building & rehabilitating the nation's affordable housing developments, also creating incentive funds for cities to invest further in those developments.<br/><br/>Finally, her policy is the only one to propose a sufficient plan for covering the cost of these benefits, by restoring Bush Administration-era estates taxes.</p>
       </React.Fragment>
     )
   }
