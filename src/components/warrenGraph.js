407652dc9935f8a1c42acc5f4f1da5d5a1f3b223
Andrew Johnson <ajohnson.uva@gmail.com>
2019-05-15 14:36:26 -0400

    add Table component for WarrenGraph that visualizes examples of govt-paid downpayments

--- src/components/warrenGraph.js
+++ src/components/warrenGraph.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
-import '../../node_modules/react-vis/dist/style.css';
-import {XYPlot, LineSeries} from 'react-vis';
+// import '../../node_modules/react-vis/dist/style.css';
+// import {XYPlot, LineSeries} from 'react-vis';
+import { Table } from 'react-bootstrap'
 
 class WarrenGraph extends Component {
 
+  displayNum = num => num.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+
   render() {
-    return(<h2>WarrenGraph</h2>)
+    const salaryNum = this.props.graphData[0]
+    const [house1, benefit1, house2, benefit2, house3, benefit3] = [
+      (Math.round(salaryNum * 3.5)).toString(),
+      (Math.round(0.035 * (salaryNum * 3.5))).toString(),
+      (Math.round(salaryNum * 4.5)).toString(),
+      (Math.round(0.035 * (salaryNum * 4.5))).toString(),
+      (Math.round(salaryNum * 5.5)).toString(),
+      (Math.round(0.035 * (salaryNum * 5.5))).toString()
+    ]
+    return(
+      <Table bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>Property Value<br/>of a New Home</th>
+            <th>Govt-Assistance<br/>for Downpayment</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>${ this.displayNum(house1) }</td>
+            <td>${ this.displayNum(benefit1) }</td>
+          </tr>
+          <tr>
+            <td>${ this.displayNum(house2) }</td>
+            <td>${ this.displayNum(benefit2) }</td>
+          </tr>
+          <tr>
+            <td>${ this.displayNum(house3) }</td>
+            <td>${ this.displayNum(benefit3) }</td>
+          </tr>
+        </tbody>
+      </Table>
+    )
   }
 }
 
